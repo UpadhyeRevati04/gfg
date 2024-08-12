f4ac5f64acdc03ad894be7d77c3831e6cd6b4646
Revati Upadhye <149503560+UpadhyeRevati04@users.noreply.github.com>
2024-08-12 14:52:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common Elements/common-elements.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking total count-1 of elements 
+		    int n=sc.nextInt();
+		    
+		    //Declaring and Initializing an ArrayList-1
+		    int v1[]=new int[n];
+		    
+		    //adding elements to the ArrayList-1
+		    for(int i=0;i<n;i++)
+		    {
+		        v1[i]=sc.nextInt();
+		    }
+		    
+		    //taking total count-2 of elements 
+		    int m=sc.nextInt();
+		    
+		    //Declaring and Initializing an ArrayList-2
+		    int v2[]=new int[m];
+		    
+		    //adding elements to the ArrayList-2
+		    for(int i=0;i<m;i++)
+		    {
+		        v2[i]=sc.nextInt();
+		    }
+		    Solution ob = new Solution();
+		    //calling the method common_element
+		    //and passing ArrayList 1, 2 as arguments
+		    //and storing the results in a new ArrayList
+		    ArrayList<Integer>ans=ob.common_element(v1, v2);
+		    
+		    //printing the elements of the new ArrayList
+		    for(int i:ans)
+		    System.out.print(i+" ");
+		    
+		    System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static ArrayList<Integer> common_element(int v1[], int v2[])
+    {
+        //Your code here
+        int n = v1.length;
+        int m = v2.length;
+        int count = 0;
+        int size = n+m;
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : v1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Finding common elements in v2
+        for (int num : v2) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1); // Decrement the count
+            }
+        }
+
+        // Sorting the result
+        Collections.sort(result);
+        return result;
+    }
+}
